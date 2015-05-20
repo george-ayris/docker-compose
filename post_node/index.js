50266534fd585ea4a6dd572f5ee00e9568e4303e
George Ayris <george.ayris@huddle.com>
2015-05-20 15:42:03 +0100

    Simplified nginx config

--- post_node/index.js
+++ post_node/index.js
@@ -15,7 +15,6 @@ console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_
 // APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
 var client = redis.createClient('6379', 'redis');
 
-
 app.post('/', function(req, res, next) {
   client.incr('counter', function(err, counter) {
     if(err) return next(err);
